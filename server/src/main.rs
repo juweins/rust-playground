2116d8f5872b17dfbf9489b4f37d956212d6ab35
juweins <julian.weins@gmx.de>
2023-01-05 21:14:56 +0100

    Learning: modularisation in rust

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,46 +1,50 @@
-fn main() {
+use http::request::Request;
+use server::HTTPServer;
 
+fn main() {
     // The server should listen on localhost:8080 by default
     let server = HTTPServer::new("127.0.0.1:8080".to_string());
     server.run();
-
 }
 
-struct HTTPServer {
-    ip_address: String,
+mod server {
+    pub struct HTTPServer {
+        ip_address: String,
+    }
+
+    impl HTTPServer {
+        // Self is a special keyword, which is an alias for the structs' name
+        pub fn new(ip_address: String) -> Self {
+            Self { ip_address }
+        }
+
+        // This is a method which uses self reference
+        pub fn run(self) {}
+    }
 }
 
-impl HTTPServer {
-
-    // Self is a special keyword, which is an alias for the structs' name
-    fn new(ip_address: String) -> Self{
-        Self{
-            ip_address
+// This is a module with submodules
+mod http {
+    pub mod request {
+        use super::method::Method;
+        pub struct Request {
+            path: String,
+            method: Method,
+            query_string: Option<String>,
         }
     }
 
-    // This is a method which uses self reference
-    fn run(self) {
-
+    pub mod method {
+        pub enum Method {
+            GET,
+            POST,
+            DELETE,
+            CONNECT,
+            HEAD,
+            OPTIONS,
+            PATCH,
+            PUT,
+            TRACE,
+        }
     }
-}
-
-// Options are a way to deal with the absence of a value.
-// (in our case, if we would pass no query string)
-struct HTTPRequest {
-    path: String,
-    method: Method,
-    query_string: Option<String>,
-}
-
-enum Method {
-    GET,
-    POST,
-    DELETE,
-    CONNECT,
-    HEAD,
-    OPTIONS,
-    PATCH,
-    PUT,
-    TRACE
 }
