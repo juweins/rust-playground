2f8c22fa194961219f201706a8dbd79b241ebcb4
juweins <julian.weins@gmx.de>
2023-01-07 23:17:56 +0100

    Learning: Traits, unimplemented and arrays

--- server/src/http/request.rs
+++ server/src/http/request.rs
@@ -4,3 +4,12 @@ pub struct Request {
     method: Method,
     query_string: Option<String>,
 }
+
+impl TryFrom<&[u8]> for Request {
+    type Error = String;
+
+    // example incoming Request: GET /search?name=sometext&sort=1 HTTP/1.1
+    fn try_from(buffer: &[u8]) -> Result<Self, Self::Error> {
+        unimplemented!()
+    }
+}
--- server/src/server.rs
+++ server/src/server.rs
@@ -1,4 +1,7 @@
+use crate::http::Request;
+use std::convert::TryFrom;
 use std::net::TcpListener;
+use std::io::Read;
 
 pub struct HTTPServer {
     ip_address: String,
@@ -18,8 +21,19 @@ impl HTTPServer {
 
         loop {
             match listener.accept() {
-                Ok((stream, address)) => {
-                    //TODO: Implement functionality
+                Ok((mut stream, address)) => {
+                    let mut buffer = [0u8; 1024];
+                    match stream.read(&mut buffer){
+                        Ok(_) => {
+                            println!("Received request: {}", String::from_utf8_lossy(&buffer));
+
+                            Request::try_from(&buffer[..]);
+
+                        }
+                        Err(e) => {
+                            println!("Failed reading stream: {}", e)
+                        }
+                    }
                 }
                 Err(e) => {
                     println!("Failed {}", e);
