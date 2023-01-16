08fb3366034746c3f490aee6640667f0ff15ec83
juweins <julian.weins@gmx.de>
2023-01-16 15:39:44 +0100

    learning: routing incoming requests with handler

--- server/src/http/request.rs
+++ server/src/http/request.rs
@@ -21,6 +21,22 @@ pub struct Request<'buffer> {
     query: Option<QueryString<'buffer>>,
 }
 
+impl <'buffer>Request<'buffer>{
+
+    pub fn path(&self) -> &str {
+        &self.path
+    }
+    
+    pub fn method(&self) -> &Method {
+        &self.method
+    }
+
+    pub fn query_string(&self) -> Option<&QueryString> {
+            self.query.as_ref()
+        }
+}
+
+
 impl<'buffer> TryFrom<&'buffer[u8]> for Request<'buffer> {
     type Error = ParseError;
 
--- server/src/website_handler.rs
+++ server/src/website_handler.rs
@@ -1,10 +1,20 @@
 use super::server::Handler;
-use super::http::{Response, Request, StatusCode};
+use super::http::{Response, Request, StatusCode, Method};
 
 pub struct WebsiteHandler;
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
-        Response::new(StatusCode::Ok, Some("<h1>Hello Traits!</h1>".to_string()))
+        match request.method() {
+            Method::GET => match request.path() {
+                "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome to my Rust server<h1>".to_string())),
+                "/main" => Response::new(StatusCode::Ok, Some("<h1>This is a path destination<h1>".to_string())),
+                _ => Response::new(StatusCode::NotFound, None),
+            },
+
+            _ => Response::new(StatusCode::NotFound, None),
+
+        }
+
     }
 }
