c827d4f82cc93d6b59632d714c0e0193e7291682
juweins <julian.weins@gmx.de>
2023-01-16 16:53:55 +0100

    learning: dynamic html file serving

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,7 +2,9 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
 
-
+// builtin
+use std::env;
+// custom
 use http::Method;
 use http::Request;
 use server::HTTPServer;
@@ -14,7 +16,12 @@ mod http;
 mod website_handler;
 
 fn main() {
+
+    // using the cargo_manifest_dir will return the pwd for default_path
+    let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
+
     // The server should listen on localhost:8080 by default
     let server = HTTPServer::new("127.0.0.1:8080".to_string());
-    server.run(WebsiteHandler);
+    server.run(WebsiteHandler::new(public_path));
 }
--- server/src/website_handler.rs
+++ server/src/website_handler.rs
@@ -1,20 +1,55 @@
 use super::server::Handler;
 use super::http::{Response, Request, StatusCode, Method};
 
-pub struct WebsiteHandler;
+use std::fs;
+
+pub struct WebsiteHandler{
+    public_path: String
+}
+
+impl WebsiteHandler {
+    pub fn new(public_path: String) -> Self {
+        Self {public_path}
+    }
+
+    fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}/{}", self.public_path, file_path);
+
+        match fs::canonicalize(path) {
+            Ok(path) => {
+                if path.starts_with(&self.public_path) {
+                    fs::read_to_string(path).ok()
+                } else {
+                    println!("Directory traversal attack attempted: {}", file_path);
+                    None
+                }
+            }
+            Err(_) => None
+        }
+    }
+}
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
         match request.method() {
             Method::GET => match request.path() {
-                "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome to my Rust server<h1>".to_string())),
-                "/main" => Response::new(StatusCode::Ok, Some("<h1>This is a path destination<h1>".to_string())),
-                _ => Response::new(StatusCode::NotFound, None),
+                "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
+                path => match self.read_file(path) {
+                    Some(contents) => Response::new(StatusCode::Ok, Some(contents)),
+                    None => Response::new(StatusCode::NotFound, None),
+                }
             },
-
             _ => Response::new(StatusCode::NotFound, None),
 
+        
+            Method::POST => todo!(),
+            Method::DELETE => todo!(),
+            Method::CONNECT => todo!(),
+            Method::HEAD => todo!(),
+            Method::OPTIONS => todo!(),
+            Method::PATCH => todo!(),
+            Method::PUT => todo!(),
+            Method::TRACE => todo!(),
         }
-
     }
 }
