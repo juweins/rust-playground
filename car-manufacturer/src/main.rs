551d84c508b22e1b0483f3a368083ef5887054b3
juweins <julian.weins@gmx.de>
2023-01-19 15:29:17 +0100

    Added: HashMap to store orders

--- car-manufacturer/src/main.rs
+++ car-manufacturer/src/main.rs
@@ -1,6 +1,7 @@
 // This is my solution for the coding exercise
 
 use rand::Rng;
+use std::collections::HashMap;
 
 fn main(){
 
@@ -10,11 +11,16 @@ let mut colors = ["blue", "green", "red", "silver"];
 // cast length of colors array to an integer
 let colors_len = colors.len() as u32;
 
+// Initialize a hash map to store the cars
+// - Key: Car order_number, i32
+// - Value: Car properties, Car struct
+let mut orders: HashMap<u32, Car> = HashMap::new();
+
 // We have orders for three new cars!
 // We'll declare a mutable car variable and reuse it for all the cars
     // Create a new random configuration for each car
-    // example: [2, 1, 0, 3000] = [color, transmission, convertible, mileage]
-    // input: [color, mileage] (transmission, convertible) are fixed
+    // example: [2, Transimission, Roof, 3000] = [color, transmission, convertible, mileage]
+    // input: [color, mileage] (transmission, convertible) are fixed enums
 
 
     // Create new cars with random specs based on user input
@@ -25,7 +31,8 @@ let colors_len = colors.len() as u32;
 
     std::io::stdin().read_line(&mut input).expect("Failed to read line");
     let mut input: u32 = input.trim().parse().expect("Please provide the amount of cars (Integer)!");
-
+    
+    let mut order_count: u32 = 0;
 
     let mut configuration = random_configuration(colors_len, 10000);
 
@@ -34,7 +41,7 @@ let colors_len = colors.len() as u32;
     let mut convertible = configuration.2;
     let mut age = car_quality(configuration.3);
 
-    while input > 0 {
+    while order_count < input {
         let car = car_factory(color, engine, convertible, age);
         println!("Car is ready!");
         configuration = random_configuration(colors_len, 10000);
@@ -42,8 +49,13 @@ let colors_len = colors.len() as u32;
         engine = configuration.1;
         convertible = configuration.2;
         age = car_quality(configuration.3);
-        input -= 1;
+        order_count += 1;
+        orders.insert(order_count, car);
     }
+
+    println!("Order processing complete!");
+    print!("-------------------------\n");
+    print_lot(&orders);
 
     
 }
@@ -155,4 +167,10 @@ fn random_configuration(max_color: u32, max_milage: u32) -> (u32, Transmission, 
     };
 
     (random_numbers[0], transmission_type, roof_type, random_numbers[2])
+}
+
+fn print_lot(orders: &HashMap<u32, Car>){
+    for (key, value) in orders.iter() {
+        println!("{}: {:?}", key, value);
+    }
 }
