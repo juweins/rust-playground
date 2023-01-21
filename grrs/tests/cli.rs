3790b72ddd3e715c03ce8791fa753bab0cfeba93
juweins <julian.weins@gmx.de>
2023-01-22 00:17:48 +0100

    Added: Integration tests to reflect new functionalities

--- grrs/tests/cli.rs
+++ grrs/tests/cli.rs
@@ -1,7 +1,8 @@
-use assert_cmd::prelude::*; // Add methods on commands
-use assert_fs::prelude::*; // Used for writing files
+use assert_cmd::{prelude::*, assert}; // Add methods on commands
+use assert_fs::prelude::*; use grrs::search_file;
+// Used for writing files
 use predicates::prelude::*; // Used for writing assertions
-use std::process::Command; // Run programs
+use std::{process::Command}; // Run programs
 
 
 // Test if the program exits with an error if no arguments are provided
@@ -26,16 +27,19 @@ fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
 // - Run the program
 // - Assert that the program exits successfully
 // - Assert that the error message contains the expected message
+// TODO: Find a way to make this test more readable without formatting the output
 #[test]
 fn find_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
     let file = assert_fs::NamedTempFile::new("sample.txt")?;
     file.write_str("A test\nActual content\nMore content\nAnother test")?;
 
+    // test if the file contains the expected content
     let mut cmd = Command::cargo_bin("grrs")?;
     cmd.arg("test").arg(file.path());
     cmd.assert()
         .success()
-        .stdout(predicate::str::contains("test\nAnother test"));
+        .stdout(predicate::str::contains("A \x1b[1;31mtest\x1b[0m"))
+        .stdout(predicate::str::contains("Another \x1b[1;31mtest\x1b[0m"));
 
     Ok(())
 }
